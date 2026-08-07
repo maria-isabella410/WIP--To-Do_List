7f667b7fa517b1b0f781a82b3521dbf63eb4fdd1
Maria Isabella <isabelladefranca410@gmail.com>
2026-08-07 16:01:35 -0300

    add js

---
+++ js/main.js
@@ -0,0 +1,78 @@
+const topicos = document.querySelector(".topicos");
+
+const esquerda = document.querySelector(".esquerda")
+
+const cadastrarTarefa = document.getElementById("cadastrar_tarefa");
+const visualizarTarefa = document.getElementById("visualizar_tarefas");
+const editarTarefa = document.getElementById("editar_tarefa");
+const excluirTarefa = document.getElementById("excluir_tarefa");
+
+const form = document.querySelector(".formulario");
+const agenda = document.getElementById("tarefas_atuais");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const tituloTarefa = document.getElementById("titulo").value;
+    const descricaoTarefa = document.getElementById("descricao").value;
+    const tarefaAtual = document.createElement("li");
+
+    tarefaAtual.textContent = tituloTarefa;
+
+    agenda.appendChild(tarefaAtual);
+});
+
+let visualizarAtivo = false;
+
+visualizarTarefa.addEventListener("click", function(event){
+    event.preventDefault();
+
+    
+    if (!visualizarAtivo){
+        visualizarTarefa.addEventListener("click", function(event){
+        event.preventDefault();
+
+        topicos.classList.add("mostrar_agenda");
+        
+        visualizarAtivo = true;
+        });
+    }
+
+    if (visualizarAtivo){
+        visualizarTarefa.addEventListener("click", function(event){
+        event.preventDefault();
+
+        topicos.classList.remove("mostrar_agenda");
+        
+        visualizarAtivo = false;
+        });
+    }
+
+});
+
+let cadastrarAtivo = false;
+
+cadastrarTarefa.addEventListener("click", function(event){
+
+    event.preventDefault();
+
+    if(!cadastrarAtivo){
+        cadastrarTarefa.addEventListener("click", function(event){
+        event.preventDefault();
+        
+        esquerda.classList.add("mostrar_form");
+
+        cadastrarAtivo = true;
+        });
+    }
+
+    if(cadastrarAtivo){
+        cadastrarTarefa.addEventListener("click", function(event){
+        event.preventDefault();
+        
+        esquerda.classList.remove("mostrar_form");
+
+        cadastrarAtivo = false;
+        });
+    }
+});
