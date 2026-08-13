ec6d78074465f3adb19d7c79e9bcd3442726722a
Maria Isabella <isabelladefranca410@gmail.com>
2026-08-13 14:03:33 -0300

    altera js e css

--- js/main.js
+++ js/main.js
@@ -18,43 +18,31 @@ cadastrarTarefa.addEventListener("click", function(event){
     caixaForm.classList.toggle("ativa");
 });
 
-let visualizarAtivo = false;
-
 visualizarTarefa.addEventListener("click", function(event){
     event.preventDefault();
 
-    
-    if (!visualizarAtivo){
-        visualizarTarefa.addEventListener("click", function(event){
-        event.preventDefault();
-
-        topicos.classList.add("mostrar_agenda");
-        
-        visualizarAtivo = true;
-        });
-    }
-
-    if (visualizarAtivo){
-        visualizarTarefa.addEventListener("click", function(event){
-        event.preventDefault();
-
-        topicos.classList.remove("mostrar_agenda");
-        
-        visualizarAtivo = false;
-        });
-    }
-
+    topicos.classList.toggle("mostrar_agenda");
 });
+
+const tituloTarefa = document.getElementById("titulo").value;
+const descricaoTarefa = document.getElementById("descricao").value;
+const prioridadeTarefa = document.getElementById("prioridade").value;
+const tarefaAtual = document.createElement("li");
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
-    const tituloTarefa = document.getElementById("titulo").value;
-    const descricaoTarefa = document.getElementById("descricao").value;
-    const tarefaAtual = document.createElement("li");
-
     tarefaAtual.textContent = tituloTarefa;
 
     agenda.appendChild(tarefaAtual);
+});
+
+agenda.addEventListener("click", function(event){
+    event.preventDefault();
+
+    const tituloInfoTarefa = document.createElement("h2");
+    const descInfoTarefa = document.createElement("h2");
+
+    //falta terminar isso
 });
 
