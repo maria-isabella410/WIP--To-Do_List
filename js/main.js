b5d83d90bb99456f57877d875f4fea61675bd220
Maria Isabella <isabelladefranca410@gmail.com>
2026-08-14 15:21:49 -0300

    corrige js

--- js/main.js
+++ js/main.js
@@ -24,25 +24,25 @@ visualizarTarefa.addEventListener("click", function(event){
     topicos.classList.toggle("mostrar_agenda");
 });
 
-const tituloTarefa = document.getElementById("titulo").value;
-const descricaoTarefa = document.getElementById("descricao").value;
-const prioridadeTarefa = document.getElementById("prioridade").value;
-const tarefaAtual = document.createElement("li");
-
 form.addEventListener("submit", function(event){
     event.preventDefault();
+
+    const tituloTarefa = document.getElementById("titulo").value;
+    const descricaoTarefa = document.getElementById("descricao").value;
+    const prioridadeTarefa = document.getElementById("prioridade").value;
+    const tarefaAtual = document.createElement("li");
 
     tarefaAtual.textContent = tituloTarefa;
 
     agenda.appendChild(tarefaAtual);
 });
 
-agenda.addEventListener("click", function(event){
-    event.preventDefault();
+// agenda.addEventListener("click", function(event){
+//     event.preventDefault();
 
-    const tituloInfoTarefa = document.createElement("h2");
-    const descInfoTarefa = document.createElement("h2");
+//     const tituloInfoTarefa = document.createElement("h2");
+//     const descInfoTarefa = document.createElement("h2");
 
-    //falta terminar isso
-});
+//     //falta terminar isso
+// });
 
